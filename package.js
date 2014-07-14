af8d5d9dc0b902389bee8efd045641d2706fa5f8
Morten Henriksen <mh@gi2.dk>
2014-07-14 18:44:43 +0200

    initial commit

---
+++ package.js
@@ -0,0 +1,8 @@
+Package.describe({
+  version: '0.0.0',
+  summary: "Offline storage, improved. Wraps IndexedDB, WebSQL, or localStorage using a simple but powerful API."
+});
+
+Package.on_use(function (api) {
+  
+});

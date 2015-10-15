010bf4ad2d61512bf269ee5fdb891cb340e95e5d
Jason Thomas <jthomas@silvervue.com>
2015-10-15 16:40:13 -0600

    Only expose localforage to cordova.

--- package.js
+++ package.js
@@ -5,11 +5,10 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use('promise');
   api.add_files('drivers/indexeddb.js', 'client');
   api.add_files('drivers/localstorage.js', 'client');
   api.add_files('drivers/websql.js', 'client');
   api.add_files('localforage.js', 'client');
 
-  api.export('localForage', 'client');
+  api.export('localForage', 'web.cordova');
 });
